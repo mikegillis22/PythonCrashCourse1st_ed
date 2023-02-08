16cfd8257a798a4e8d2edc7f28cb1b2ef9ffb66f
Mike Gillis <mikegillis22@gmail.com>
2023-02-08 14:42:03 -0500

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,19 @@
+#print("Hello Python world!")
+
+#message = "Hello Python Crash Course World!"
+#print(message)
+
+#name="mike gillis"
+#print(name)
+#print(name.upper())
+#print(name.title())
+
+first_name = "  mike"
+last_name = "gillis  "
+full_name = first_name + " " + last_name
+print(full_name)
+print(full_name.title())
+print(full_name.upper())
+print("Hello, " + full_name.title().lstrip() + "!")
+message = "\tHello, " + full_name.title().strip() + "!"
+print(message)
