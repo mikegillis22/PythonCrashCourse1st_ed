a98c69881d106f77b3db04be4ad504148c7274c4
Mike Gillis <mikegillis22@gmail.com>
2023-02-09 12:55:46 -0500

    Chapter 2 exercises

---
+++ Chapter2.py
@@ -0,0 +1,40 @@
+# Python Crash Course Chaper 2 exercises
+
+#2-1
+greeting = "Welcome to Python!"
+print ("2-1\t"+greeting)
+#2-2
+greeting = "Having fun yet?"
+print ("2-2\t"+greeting)
+#2-3
+name="Mike"
+print ("2-3\t"+"Hello "+name+", would you like to learn some Python today?")
+#2-4
+name="JACk SpaRRow"
+print ("2-4\t"+name.upper())
+print ("2-4\t"+name.lower())
+print ("2-4\t"+name.title())
+#2-5
+print ('2-5\tPresident Kennedy once said, "We choose to go to the moon!"')
+famous_person="Neil Armstrong"
+#2-6
+print ("2-6\t"+famous_person+" once said, "+'"That'+"'s"+' one small step for a man,\n\tone giant leap for mankind!"')
+#2-7
+name= "  Jack Sparrow  "
+print ("2-7\t"+name)
+print("2-7\t"+name.lstrip())
+print("2-7\t"+name.rstrip())
+print("2-7\t"+name.strip())
+#2-8
+firstNum = 3+5
+print("2-8\tFirst Number: "+ str(firstNum))
+secondNum = 16-8
+print("2-8\tSecond Number: "+ str(secondNum))
+thirdNum = 2*4
+print("2-8\tThird Number: "+ str(thirdNum))
+fourthNum = 24/3
+print("2-8\tFourth Number: "+ str(fourthNum))
+#2-9
+fav_num=36
+message="My favorite number is "+ str(fav_num)
+print("2-9\t"+message)
