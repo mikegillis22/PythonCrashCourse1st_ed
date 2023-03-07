32dcd5f45820823c9d8273c9b1fe03282e241388
Mike Gillis <mikegillis22@gmail.com>
2023-03-07 12:24:36 -0500

    Modified style based on PEP 8

--- Chapter4-13.py
+++ Chapter4-13.py
@@ -1,6 +1,6 @@
 """
-4-13. Buffet: A buffet-style restaurant offers only five basic foods. Think of five 
-simple foods, and store them in a tuple.
+4-13. Buffet: A buffet-style restaurant offers only five basic foods. Think of 
+five simple foods, and store them in a tuple.
 •	 Use a for loop to print each food the restaurant offers.
 •	 Try to modify one of the items, and make sure that Python rejects the 
 change.
@@ -8,10 +8,13 @@ change.
 foods. Add a block of code that rewrites the tuple, and then use a for
 loop to print each of the items on the revised menu
 """
-buffet = ('mac and cheese', 'meatloaf', 'BLT', 'chicken tenders', 'cheese burger')
+buffet = ('mac and cheese', 'meatloaf', 'BLT', 'chicken tenders', 
+          'cheese burger')
 for food in buffet:
     print(food)
+
 #buffet[0] = "taco"
+
 buffet = ('taco', 'meatloaf', 'BLT', 'steak tips', 'cheese burger')
 for food in buffet:
     print(food)
