3e4dd774af98e0c7132140c30b2c26609bbf693f
Mike Gillis <mikegillis22@gmail.com>
2023-04-16 20:40:05 -0400

    Intro dictionaries

---
+++ Chapter6-1.py
@@ -0,0 +1,11 @@
+"""
+Person: Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live. You
+should have keys such as first_name, last_name, age, and city. Print each
+piece of information stored in your dictionary.
+"""
+person = {'first_name': 'jack', 'last_name': 'sparrow', 'age': '31', 'city': 'barcelona'}
+print(person['first_name'].title())
+print(person['last_name'].title())
+print(person['age'])
+print(person['city'].title())
---
+++ Chapter6-2.py
@@ -0,0 +1,29 @@
+"""
+Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a favorite
+number for each person, and store each as a value in your dictionary. Print
+each person’s name and their favorite number. For even more fun, poll a few
+friends and get some actual data for your program.
+"""
+favorite_numbers = {
+    'Mike': '22',
+    'Sam': '36',
+    'Jack': '14',
+    'Neil': '11',
+    'Pete': '1'
+}
+print("Mike's favorite number is " +
+      favorite_numbers['Mike'] +
+      ".")
+print("Sam's favorite number is " +
+      favorite_numbers['Sam'] +
+      ".")
+print("Jack's favorite number is " +
+      favorite_numbers['Jack'] +
+      ".")
+print("Neil's favorite number is " +
+      favorite_numbers['Neil'] +
+      ".")
+print("Pete's favorite number is " +
+      favorite_numbers['Pete'] +
+      ".")
